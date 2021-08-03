6214a5bd5a475658ed94043688c3c511ae389530
rrajgithub <imrishavraj2001@gmail.com>
2021-08-03 23:47:07 +0530

    alum

---
+++ stud/config/nodemailer.js
@@ -0,0 +1,10 @@
+const env = require('./environment');
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport(env.smtp);
+console.log(transporter,env.smtp);
+
+
+module.exports = {
+	transporter : transporter
+}
